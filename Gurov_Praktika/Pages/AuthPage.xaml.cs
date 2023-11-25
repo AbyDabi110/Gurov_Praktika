7135b03adc9651287e77841cfce363e4afd04e1c
smirka <smirka.n.u@gmail.com>
2023-11-25 13:08:51 +0700

    Нажал на "Вход"

--- Gurov_Praktika/Pages/AuthPage.xaml.cs
+++ Gurov_Praktika/Pages/AuthPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,15 @@ namespace Gurov_Praktika.Pages
         public AuthPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Login.Text) || string.IsNullOrEmpty(Password.Text))
+            {
+                MessageBox.Show("Введите логин и пароль.");
+                return;
+            }
         }
     }
 }
